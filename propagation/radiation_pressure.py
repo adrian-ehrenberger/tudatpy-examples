7041c9193a1ed5cb394359647d82fab0bbb5aa7d
Adrian <adrianehren@gmail.com>
2024-07-25 22:53:05 +0200

    update to rad example, start of notebook

--- propagation/radiation_pressure.py
+++ propagation/radiation_pressure.py
@@ -30,17 +30,16 @@ sys.path.insert(0, "/home/tudat-bundle/build/tudatpy")
 # Load tudatpy modules
 from tudatpy.interface import spice
 from tudatpy import numerical_simulation
-from tudatpy.numerical_simulation import environment
 from tudatpy.numerical_simulation import environment_setup, propagation_setup
 from tudatpy.astro import element_conversion
-from tudatpy import constants
 from tudatpy.util import result2array
 from tudatpy.astro.time_conversion import DateTime
 
-## Configuration
-
 # Load spice kernels
 spice.load_standard_kernels()
+
+## Configuration
+
 
 # Set simulation start and end epochs
 simulation_start_epoch = DateTime(2000, 1, 1).epoch()
